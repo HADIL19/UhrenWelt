74fb2ae0012ac15efaee4eaa8678f26640c1d60c
hadil <hadylkhelif18@gmail.com>
2025-03-18 02:48:53 +0100

    Uhren

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Lato } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const lato = Lato(
+  {
+    subsets: ["latin"],
+    weight: ["100","300","400","700" ,"900"]
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  }
+)
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,9 +23,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        <body className={`${lato.className} antialiased`}>
+        <Navbar />
         {children}
       </body>
     </html>
